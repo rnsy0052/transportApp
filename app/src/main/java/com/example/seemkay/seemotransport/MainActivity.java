ed35a23b425607f70943a40b747e8eb55d4401b4
Mohit Kumar <emkayatiitr@gmail.com>
2018-02-08 21:29:36 +0530

    Receiving data in java for username and password

--- app/src/main/java/com/example/seemkay/seemotransport/MainActivity.java
+++ app/src/main/java/com/example/seemkay/seemotransport/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.seemkay.seemotransport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method provides validation on sign on for the given user name and password.
+     * If right it navigates to the home page else produces an error message.
+     */
+    public void signOn(View view) {
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+//        Log.d("Debug",username.getText()+""+password.getText());
     }
 }
