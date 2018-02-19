e75796f5fc87562933a62a9af9669fd60fdde10f
rnsy0052 <seemayadav0052@gmail.com>
2018-02-18 19:31:51 -0800

    trying

--- app/src/main/java/com/example/seemkay/seemotransport/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/seemkay/seemotransport/ForgotPasswordActivity.java
@@ -1,7 +1,9 @@
 package com.example.seemkay.seemotransport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
@@ -10,4 +12,19 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
     }
+
+    public void resendOTP(View view) {
+    }
+
+    public void sendOTP(View view) {
+
+    }
+
+    public void confirmOTP(View view) {
+        Intent intent = new Intent(ForgotPasswordActivity.this,ResetPasswordActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
